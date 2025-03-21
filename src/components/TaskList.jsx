d2966dea9ed282b28b73801367c2371ac4d65636
lokeshloki3 <lokesh1992p@gmail.com>
2025-03-21 13:12:23 +0530

    task list with filter created

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext, useMemo, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import TaskItem from './TaskItem';
 
 const TaskList = () => {
-  return (
-    <div>TaskList</div>
-  )
+
+    const { tasks } = useContext(AppContext);
+    const [filter, setFilter] = useState("all");
+
+    const filteredTasks = useMemo(() => {
+        if (filter === "completed") {
+            return tasks.filter(task => task.completed);
+        } else if (filter === "pending") {
+            return tasks.filter(task => !task.completed);
+        }
+        return tasks;
+    }, [tasks, filter])
+
+    return (
+        <div>
+            <div>
+                <button onClick={() => setFilter("all")}>All</button>
+                <button onClick={() => setFilter("completed")}>Completed</button>
+                <button onClick={() => setFilter("pending")}>Pending</button>
+            </div>
+            {
+                filteredTasks.map((task) => {
+                    <TaskItem key={task.id} task={task} />
+                })
+            }
+        </div>
+    )
 }
 
-export default TaskList
+export default TaskList;
